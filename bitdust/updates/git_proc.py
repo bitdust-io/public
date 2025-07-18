57e9345fc6f35793d773a2898f6fa3f00064359b
Veselin Penev <penev.veselin@gmail.com>
2025-07-18 17:22:19 +0200

    blocking hire of my existing customer as my new supplier

--- bitdust/updates/git_proc.py
+++ bitdust/updates/git_proc.py
@@ -78,36 +78,42 @@ from bitdust.main import events
 #------------------------------------------------------------------------------
 
 _CurrentProcess = None
-_FirstRunDelay = 1200
-_LoopInterval = 3600*6
+_FirstRunDelay = 60*20
+_LoopInterval = 60*60*6
 _ShedulerTask = None
 
 #------------------------------------------------------------------------------
 
 
 def write2log(txt):
-    out_file = open(settings.UpdateLogFilename(), 'a')
-    out_file.write(strng.to_text(txt))
-    out_file.close()
+    try:
+        out_file = open(settings.UpdateLogFilename(), 'a')
+        out_file.write(strng.to_text(txt))
+        out_file.close()
+    except:
+        pass
 
 
 #------------------------------------------------------------------------------
 
 
 def init():
-    lg.out(4, 'git_proc.init')
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.init')
     if os.environ.get('BITDUST_GIT_SYNC_SKIP', '0') == '1':
         return
     reactor.callLater(0, loop, first_start=True)  # @UndefinedVariable
 
 
 def shutdown():
-    lg.out(4, 'git_proc.shutdown')
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.shutdown')
     global _ShedulerTask
     if _ShedulerTask is not None:
         if _ShedulerTask.active():
             _ShedulerTask.cancel()
-            lg.out(4, '    loop stopped')
+            if _Debug:
+                lg.out(_DebugLevel, '    loop stopped')
         _ShedulerTask = None
 
 
@@ -115,7 +121,8 @@ def shutdown():
 
 
 def sync_callback(result):
-    lg.out(6, 'git_proc.sync_callback: %s' % result)
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.sync_callback: %s' % result)
 
     if result == 'code-fetched':
         events.send('source-code-fetched', data=dict())
@@ -125,27 +132,29 @@ def sync_callback(result):
         events.send('source-code-update-error', data=dict(result=result))
 
     try:
-        from bitdust.system import tray_icon
+        # from bitdust.system import tray_icon
         if result == 'error':
             # tray_icon.draw_icon('error')
             # reactor.callLater(5, tray_icon.restore_icon)
             return
         elif result == 'code-fetched':
-            tray_icon.set_icon('updated')
+            # tray_icon.set_icon('updated')
             return
     except:
         pass
 
 
 def run_sync():
-    lg.out(6, 'git_proc.run_sync')
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.run_sync')
     reactor.callLater(0, sync, sync_callback, update_method='reset')  # @UndefinedVariable
     reactor.callLater(0, loop)  # @UndefinedVariable
 
 
 def loop(first_start=False):
     global _ShedulerTask
-    lg.out(4, 'git_proc.loop')
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.loop')
     if first_start:
         nexttime = time.time() + _FirstRunDelay
     else:
@@ -156,7 +165,8 @@ def loop(first_start=False):
     if delay < 0:
         lg.warn('delay=%s %s %s' % (str(delay), nexttime, time.time()))
         delay = 0
-    lg.out(6, 'git_proc.loop run_sync will start after %s minutes' % str(delay/60.0))
+    if _Debug:
+        lg.out(_DebugLevel, 'git_proc.loop run_sync will start after %s minutes' % str(delay/60.0))
     _ShedulerTask = reactor.callLater(delay, run_sync)  # @UndefinedVariable
 
 
@@ -171,50 +181,67 @@ def sync(callback_func=None, update_method='rebase'):
         `git rebase origin/master -v`  or  `git reset --hard origin/master`
 
     """
-    src_dir_path = bpio.getExecutableDir()
+    src_dir_path = os.path.abspath(os.path.join(bpio.getExecutableDir(), '..'))
     expected_src_dir = os.path.join(deploy.default_base_dir_portable(), 'src')
+    if _Debug:
+        lg.args(_DebugLevel, update_method=update_method, src_dir=src_dir_path, expected_src_dir=expected_src_dir)
     if bpio.portablePath(src_dir_path) != bpio.portablePath(expected_src_dir):
         if _Debug:
             lg.out(_DebugLevel, 'git_proc.sync SKIP, non standard sources location: %r' % src_dir_path)
         return
 
-    def _reset_done(response, error, retcode, result):
-        if callback_func is None:
-            return
-        callback_func(result)
-
-    def _rebase_done(response, error, retcode, result):
+    def _reset_done(out, err, retcode, fetch_result):
+        if _Debug:
+            lg.args(_DebugLevel, retcode=retcode, fetch_result=fetch_result)
         if callback_func is None:
             return
         if retcode != 0:
             result = 'sync-error'
         else:
-            if response.count(b'Changes from') or response.count(b'Fast-forwarded'):
+            if fetch_result == 'new-code':
                 result = 'code-fetched'
             else:
                 result = 'up-to-date'
         callback_func(result)
 
-    def _fetch_done(response, error, retcode):
+    def _rebase_done(out, err, retcode, fetch_result):
+        if _Debug:
+            lg.args(_DebugLevel, retcode=retcode, fetch_result=fetch_result)
+        if callback_func is None:
+            return
+        out = strng.to_text(out)
+        err = strng.to_text(err)
+        if retcode != 0:
+            result = 'sync-error'
+        else:
+            if fetch_result == 'new-code' or out.count('Changes from') or out.count('Fast-forwarded'):
+                result = 'code-fetched'
+            else:
+                result = 'up-to-date'
+        callback_func(result)
+
+    def _fetch_done(out, err, retcode):
+        if _Debug:
+            lg.args(_DebugLevel, retcode=retcode)
         if retcode != 0:
             if callback_func:
                 callback_func('sync-error')
             return
-        result = 'sync-error'
-        if response.count(b'Unpacking') or \
-            (response.count(b'master') and response.count(b'->')) or \
-            response.count(b'Updating') or \
-            response.count(b'Receiving') or \
-                response.count(b'Counting'):
-            result = 'new-code'
+        out = strng.to_text(out)
+        err = strng.to_text(err)
+        fetch_result = 'fetch-ok'
+        for ln in err.splitlines():
+            if ln.count('master') and ln.count('->') and not ln.count('[up to date]'):
+                fetch_result = 'new-code'
+                break
+        if out.count('Unpacking') or out.count('Updating') or out.count('Receiving') or out.count('Counting'):
+            fetch_result = 'new-code'
+        if _Debug:
+            lg.args(_DebugLevel, fetch_result=fetch_result)
         if update_method == 'reset':
-            run([
-                'reset',
-                '--hard',
-                'origin/master',
-            ], callback=lambda resp, err, ret: _reset_done(resp, err, ret, result))
+            run(['reset', '--hard', 'origin/master'], callback=lambda o, e, ret: _reset_done(o, e, ret, fetch_result))
         elif update_method == 'rebase':
-            run(['rebase', 'origin/master', '-v'], callback=lambda resp, err, ret: _rebase_done(resp, err, ret, result))
+            run(['rebase', 'origin/master', '-v'], callback=lambda o, e, ret: _rebase_done(o, e, ret, fetch_result))
         else:
             raise Exception('invalid update method: %s' % update_method)
 
@@ -227,7 +254,7 @@ def sync(callback_func=None, update_method='rebase'):
 def run(cmdargs, base_dir=None, git_bin=None, env=None, callback=None):
     if _Debug:
         lg.out(_DebugLevel, 'git_proc.run')
-    base_dir = base_dir or bpio.getExecutableDir()
+    base_dir = base_dir or os.path.abspath(os.path.join(bpio.getExecutableDir(), '..'))
     if bpio.Windows():
         cmd = [
             'git',
@@ -269,7 +296,7 @@ def execute_in_shell(cmdargs, base_dir=None):
     import subprocess
     if _Debug:
         lg.out(_DebugLevel, 'git_proc.execute_in_shell: "%s"' % (' '.join(cmdargs)))
-    write2log('EXECUTE in shell: %s, base_dir=%s' % (cmdargs, base_dir))
+    write2log('\nEXECUTE in shell: %s, base_dir=%s\n' % (cmdargs, base_dir))
     _CurrentProcess = nonblocking.Popen(
         cmdargs,
         shell=True,
@@ -281,7 +308,7 @@ def execute_in_shell(cmdargs, base_dir=None):
     result = _CurrentProcess.communicate()
     out_data = result[0]
     err_data = result[1]
-    write2log('STDOUT:\n%s\nSTDERR:\n%s\n' % (out_data, err_data))
+    write2log('STDOUT:\n%s\nSTDERR:\n%s\n' % (strng.to_text(out_data), strng.to_text(err_data)))
     returncode = _CurrentProcess.returncode
     if _Debug:
         lg.out(_DebugLevel, 'git_proc.execute_in_shell returned: %s, stdout bytes: %d, stderr bytes: %d' % (returncode, len(out_data), len(err_data)))
@@ -302,13 +329,13 @@ class GitProcessProtocol(protocol.ProcessProtocol):
         self.err += inp
         for line in inp.splitlines():
             if _Debug:
-                lg.out(_DebugLevel, '[git:err]: %s' % strng.to_text(line))
+                lg.out(_DebugLevel, '    [git:err]: %s' % strng.to_text(line))
 
     def outReceived(self, inp):
         self.out += inp
         for line in inp.splitlines():
             if _Debug:
-                lg.out(_DebugLevel, '[git:out]: %s' % strng.to_text(line))
+                lg.out(_DebugLevel, '    [git:out]: %s' % strng.to_text(line))
 
     def processEnded(self, reason):
         if _Debug:
@@ -321,7 +348,7 @@ def execute(cmdargs, base_dir=None, process_protocol=None, env=None, callback=No
     global _CurrentProcess
     if _Debug:
         lg.out(_DebugLevel, 'git_proc.execute: "%s" in %s' % (' '.join(cmdargs), base_dir))
-    write2log('EXECUTE: %s, base_dir=%s' % (cmdargs, base_dir))
+    write2log('\nEXECUTE: %s, base_dir=%s\n' % (cmdargs, base_dir))
     executable = cmdargs[0]
     if bpio.Windows():
         from twisted.internet import _dumbwin32proc
@@ -331,15 +358,15 @@ def execute(cmdargs, base_dir=None, process_protocol=None, env=None, callback=No
             import win32con  # @UnresolvedImport
             import subprocess
             flags = win32con.CREATE_NO_WINDOW
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            startupinfo.wShowWindow = subprocess.SW_HIDE
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW  # @UndefinedVariable
+            startupinfo.wShowWindow = subprocess.SW_HIDE  # @UndefinedVariable
             return real_CreateProcess(_appName, _commandLine, _processAttributes, _threadAttributes, _bInheritHandles, flags, _newEnvironment, _currentDirectory, startupinfo)
 
         setattr(_dumbwin32proc.win32process, 'CreateProcess', fake_createprocess)
 
     if process_protocol is None:
         process_protocol = GitProcessProtocol(callbacks=[
-            lambda out, err, ret_code: write2log('STDOUT:\n%s\nSTDERR:\n%s\n' % (out, err)),
+            lambda out, err, ret_code: write2log('STDOUT:\n%s\nSTDERR:\n%s\n' % (strng.to_text(out), strng.to_text(err))),
             callback,
         ])
     try:
